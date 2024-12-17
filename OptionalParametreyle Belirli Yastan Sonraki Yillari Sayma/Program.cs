70dce1cef140ccd557e03f2f1ea9b605d443f375
yaren <yaren@DESKTOP-9U2BG79>
2024-12-18 02:49:53 +0300

    Add project files.

---
+++ OptionalParametreyle Belirli Yastan Sonraki Yillari Sayma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParametreyle_Belirli_Yastan_Sonraki_Yillari_Sayma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Varsayılan Yaşı 18 yerine kendi yaşım 20 olarak aldım
+
+            Console.WriteLine("Yaş Belirtilmedi: " + YasFarki() + " yaş büyüktür"); //Bu örnekte yaşı belirtmediğim için varsayılan yaş hesaba katılacaktır.
+
+            Console.WriteLine("Yaş 56: " + YasFarki(56) + " yaş büyüktür");
+
+            Console.WriteLine("Yaş 52: " + YasFarki(52) + " yaş büyüktür");
+
+            Console.WriteLine("Yaş 28: " + YasFarki(28) + " yaş büyüktür");
+
+            Console.ReadKey();
+
+        }
+
+        static int YasFarki(int yas = 20)
+        {
+            return yas - 20;
+        }
+
+    }
+}
---
+++ OptionalParametreyle Belirli Yastan Sonraki Yillari Sayma/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OptionalParametreyle Belirli Yastan Sonraki Yillari Sayma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OptionalParametreyle Belirli Yastan Sonraki Yillari Sayma")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8916b654-a151-4c8a-831d-3d62f45e2710")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
